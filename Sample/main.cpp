7c43a0356198f6f18af9d6753bd029c8032ba94e
RaviDesai <ravi@Ravis-MacBook-Pro.local>
2013-11-23 12:02:48 -0500

    Initial Commit

---
+++ Sample/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sample
+//
+//  Created by Ravi Desai on 11/23/13.
+//  Copyright (c) 2013 Ravi Desai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
